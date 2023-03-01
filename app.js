6f908783d2759bad9fff6edd34577e0f808c5514
VR-Store_Neural <vrstoreneural@gmail.com>
2023-03-01 17:45:37 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express') // отримання пакету express
+const config = require('config') // для отримання констант з config
+const mongoose = require('mongoose') // для підключення до MongoDB
+
+const app = express() // наш серевер
+
+const PORT = config.get('port') || 5000 // для отримання порту 
+
+async function start() {     // тому, що метод .connect повертає promise, обертаємо у функцію async/await
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            // useCreateIndex: true
+        })
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`)) // запуск серверу на порті 5000
+    } catch (err) {
+        console.log('Server Error', err.message)
+        process.exit(1)
+    }
+}
+
+start()
+
